cdc0c050e97985b28a8a0e872db8bc55af3afbbc
Joe Haines <joehaines26@gmail.com>
2026-04-08 14:58:25 +0100

    Add submission zip

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,7 @@ FROM pklehre/ec2025-lab2
 RUN apt-get update
 RUN apt-get -y install python3-numpy python3-matplotlib
 
-COPY ./common /bin/common
-COPY ./questions /bin/questions
+COPY ./src /bin/src
 COPY ./main.py /bin/main.py
 
 CMD ["-username", "jfh245", "-submission", "python3 /bin/main.py"]
--- src/questions/q2.py
+++ src/questions/q2.py
@@ -1,14 +1,5 @@
 from src.common.maxsat import parse_wdimacs,parse_assignment_str,n_sat
 
-# Exercise 2. (10 % of the marks) Implement a routine for importing MAXSAT instances on the
-# WDIMACS file format. A WDIMACS file is a text file where each line either
-# • begins with the letter c, indiciating that the rest of the line is a comment
-# • begins with p cnf n m x where n is the number of variables, m is the number of clauses, and
-# x is an optional parameter which may or may not be present, or
-# • describes a clause, as explained in Exercise 1.
-# To test that the import routine works correctly, you should count the number of clauses which are
-# satisfied by a given assignment.
-
 def run(wdimacs, assignment_str):
 
   n,m,clauses = parse_wdimacs(wdimacs)
